eab89ca178e56f5932e939c6fa55a5a98bf65d78
Likhith <likhith.a@way2news.com>
2023-03-16 15:40:54 +0530

    changed accordingly for external tomcat deployment.

--- src/main/java/com/h2example/crudlogin/CrudloginApplication.java
+++ src/main/java/com/h2example/crudlogin/CrudloginApplication.java
@@ -2,9 +2,10 @@ package com.h2example.crudlogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrudloginApplication {
+public class CrudloginApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudloginApplication.class, args);
