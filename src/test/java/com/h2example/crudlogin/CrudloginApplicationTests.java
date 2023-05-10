5bb609b772e7fb6eff8c34783bff7e5720797959
Likhith <likhith.a@way2news.com>
2023-05-10 10:54:23 +0530

    Added half baked test case implementation

--- src/test/java/com/h2example/crudlogin/CrudloginApplicationTests.java
+++ src/test/java/com/h2example/crudlogin/CrudloginApplicationTests.java
@@ -1,5 +1,7 @@
 package com.h2example.crudlogin;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class CrudloginApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("test begin..............");
+		assertEquals(5, 3+2);
 	}
 
 }
