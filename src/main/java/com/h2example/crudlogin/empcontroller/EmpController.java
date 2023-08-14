1bfd4666c3cc67db73892e9e33baafdeff81c3d8
Likhith <likhith.a@way2news.com>
2023-08-14 16:23:12 +0530

    placeholders for another db configuration has been deleted to restructure project arch for multiple dbs

--- src/main/java/com/h2example/crudlogin/empcontroller/EmpController.java
+++ src/main/java/com/h2example/crudlogin/empcontroller/EmpController.java
@@ -1,22 +1,14 @@
-package com.h2example.crudlogin.Controllers.empcontroller;
+package com.h2example.crudlogin.empcontroller;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.h2example.crudlogin.Entities.entity.EmpEntity;
+import com.h2example.crudlogin.Repositories.emprepository.EmpRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.h2example.crudlogin.Repositories.emprepository.EmpRepo;
-import com.h2example.crudlogin.Entities.entity.EmpEntity;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/emp")
